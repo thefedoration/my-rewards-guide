ec82836a5d9d34d8a19b161071b385e58c607fdb
Fedor Garin <fedor.garin@gmail.com>
2016-07-24 20:26:11 -0400

    initial app setup

---
+++ myrewardsguide/myrewardsguide/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+"""
+LANDING PAGE
+"""
+def home(request):
+	return render(request, 'home/index.html')
+
+
+def handler404(request):
+	return render(request, '404.html', status=404)
+
+
+def handler500(request):
+	return render(request, '500.html', status=500)
